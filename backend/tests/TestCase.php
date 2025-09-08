127fc136295a3449f5c3583ce32850b5b0b3b61a
Schäffer Gergő <58182569+nemjoanevem@users.noreply.github.com>
2025-09-08 16:51:06 +0200

    chore(test): run tests on test db

--- backend/tests/TestCase.php
+++ backend/tests/TestCase.php
@@ -3,8 +3,23 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\DB;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        if (!app()->environment('testing')) {
+            throw new \RuntimeException('Tests must run in testing environment.');
+        }
+
+        $db = DB::select('select current_database() as db')[0]->db ?? '';
+        if (strpos($db, 'test') === false) {
+            throw new \RuntimeException("Unsafe test database name: {$db}");
+        }
+    }
 }
