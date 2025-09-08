3c1eb0f7e823e09e58ad51c41f460fe731bd6301
Schäffer Gergő <58182569+nemjoanevem@users.noreply.github.com>
2025-09-08 18:11:20 +0200

    feat(db): add guest_name and guest_email to comments

---
+++ backend/database/migrations/2025_09_08_160152_allow_guest_comments.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        DB::statement('ALTER TABLE comments ALTER COLUMN user_id DROP NOT NULL');
+
+        Schema::table('comments', function (Blueprint $table) {
+            $table->string('guest_name', 100)->nullable()->after('user_id');
+            $table->string('guest_email')->nullable()->after('guest_name');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropColumn(['guest_name','guest_email']);
+        });
+
+        if (DB::getDriverName() === 'pgsql') {
+            DB::statement('ALTER TABLE comments ALTER COLUMN user_id SET NOT NULL');
+        }
+    }
+};
